7dcd77bb20977459b05f6e4fed60f769edd642dc
Davee Sok <daveesok@gmail.com>
2020-03-18 21:28:29 -0700

    updated text positioning

--- interactive.js
+++ interactive.js
@@ -1,7 +1,19 @@
+var txt;
+var person = prompt("Please enter your name");
+if (person == null || person == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + person + "! How are you today?";
+}
+document.write('<h1>' + txt + '</h1>');
+
+var answer = prompt("How many bags of chips a day do you eat?");
+var chipAmount = "We're glad you eat " + answer + " bags of chips a day!!" 
+document.write('<h2>' + chipAmount + '</h2>');
+
 var today = new Date();
 var hourNow = today.getHours();
 var greetings;
-
 if (hourNow > 18) {
     greeting = 'Have some Chips with Dinner!';
 } else if (hourNow > 12) {
@@ -11,20 +23,4 @@ if (hourNow > 18) {
 } else {
     greeting = 'Welcome';
 }
-document.write('<h3>' + greeting + '</h3>');
-
-
-var txt;
-var person = prompt("Please enter your name");
-if (person == null || person == "") {
-  txt = "User cancelled the prompt.";
-} else {
-  txt = "Hello " + person + "! How are you today?";
-}
-document.write('<h3>' + txt + '</h3>');
-
-var answer = prompt("How many bags of chips a day do you eat?");
-
-
-var chipAmount = "We're glad you eat " + answer + " bags of chips a day!!" 
-document.write('<h3>' + chipAmount + '</h3>');
+document.write('<h2>' + greeting + '</h2>');
