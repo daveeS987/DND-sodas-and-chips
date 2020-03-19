caedcad579a3b690a0277058f90945493336a70a
Davee Sok <daveesok@gmail.com>
2020-03-19 15:53:01 -0700

    updated

--- interactive.js
+++ interactive.js
@@ -1,28 +1,34 @@
-var txt;
-var person = prompt("Please enter your name");
-if (person == null || person == "") {
-  txt = "User cancelled the prompt.";
-} else {
-  txt = "Hello " + person + "! How are you today?";
+function intialGreeting() {
+  var person = prompt("Please enter your name");
+  var txt = "Hello " + person + "! How are you today?";
+  alert(txt);
+  // document.write('<h1>' + txt + '</h1>');
 }
-document.write('<h1>' + txt + '</h1>');
 
-
-var answer = prompt("How many bags of chips a day do you eat?");
-var chipAmount = "We're glad you eat " + answer + " bags of chips a day!!" 
-document.write('<h2>' + chipAmount + '</h2>');
-
-
-var today = new Date();
-var hourNow = today.getHours();
-var greetings;
-if (hourNow > 18) {
-    greeting = 'Have some Chips with Dinner!';
-} else if (hourNow > 12) {
-    greeting = 'Have an afternoon snack!';
-} else if (hourNow > 0) {
-    greeting = 'Breakfast for champions!';
-} else {
-    greeting = 'Welcome';
+function chipsMessage() {
+  var answer = prompt("How many bags of chips a day do you eat?");
+  var chipAmount = "We're glad you eat " + answer + " bags of chips a day!!" 
+  document.write('<h2>' + chipAmount + '</h2>');
 }
-document.write('<h2>' + greeting + '</h2>');
+
+function timeBasedMsg() {
+  var today = new Date();
+  var hourNow = today.getHours();
+  var greetings;
+  if (hourNow > 18) {
+      greeting = 'Have some Chips with Dinner!';
+  } else if (hourNow > 12) {
+      greeting = 'Have an afternoon snack!';
+  } else if (hourNow > 0) {
+      greeting = 'Breakfast for champions!';
+  } else {
+      greeting = 'Welcome';
+  }
+  document.write('<h2>' + greeting + '</h2>');
+}
+// timeBasedMsg();
+
+// function getFavoriteChip() {
+//   var favoriteChip = prompt("Which do you prefer? doritos, pringles, or cheetos?");
+
+// }
